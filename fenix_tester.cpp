7cdf509bb288b1d19b8212fc5d0c35a1c1a1f2b2
Jiří Setnička <setnicka@seznam.cz>
2015-06-18 01:21:41 +0200

    Development: Tester to test Cereal serialization

--- fenix_tester.cpp
+++ fenix_tester.cpp
@@ -1,12 +1,36 @@
+#include <iostream>
+#include <fstream>
 #include "FileTree.hpp"
+#include "FenixExceptions.hpp"
+
+#include <cereal/cereal.hpp>
+#include <cereal/archives/binary.hpp>
+#include <cereal/archives/json.hpp>
+#include <cereal/types/memory.hpp>
+
+using namespace std;
 
 int main() {
 	//auto storage = new FenixBackup::Storage();
 	//storage->Test();
 
-	auto tree = new FenixBackup::FileTree();
+	try {
+		FenixBackup::Config::Load("/home/jirka/bc_FenixBackup/test/config");
+		FenixBackup::FileTree::GetHistoryTreeList();
+        auto tree = new FenixBackup::FileTree();
+        //tree->SaveTree();
+	} catch(FenixBackup::FenixException &ex) {
+		cerr << ex.what();
+		return(EXIT_FAILURE);
+	}
 
+    std::ofstream os("test.test");
+    cereal::JSONOutputArchive archive(os);
 
+    auto FI = make_shared<FenixBackup::FileInfo>();
+    FenixBackup::FileInfo FI2;
 
-	return 0;
+    archive(FI2);
+
+	return(EXIT_SUCCESS);
 }
