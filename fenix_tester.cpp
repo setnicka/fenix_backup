e7cbf383248814edcc6d1d279e1e1c721563758b
Jiří Setnička <setnicka@seznam.cz>
2015-06-18 03:23:39 +0200

    TODOlist: Init

--- fenix_tester.cpp
+++ fenix_tester.cpp
@@ -3,10 +3,11 @@
 #include "FileTree.hpp"
 #include "FenixExceptions.hpp"
 
-#include <cereal/cereal.hpp>
+#include <cereal/types/memory.hpp>
+#include <cereal/types/polymorphic.hpp>
 #include <cereal/archives/binary.hpp>
 #include <cereal/archives/json.hpp>
-#include <cereal/types/memory.hpp>
+
 
 using namespace std;
 
@@ -18,19 +19,26 @@ int main() {
 		FenixBackup::Config::Load("/home/jirka/bc_FenixBackup/test/config");
 		FenixBackup::FileTree::GetHistoryTreeList();
         auto tree = new FenixBackup::FileTree();
-        //tree->SaveTree();
+
+        FenixBackup::file_params params = {};
+        auto f1 = tree->AddFile(tree->GetRoot(), "souborB.txt", params);
+        auto d1 = tree->AddDirectory(tree->GetRoot(), "slozka", params);
+        auto f2 = tree->AddFile(d1, "souborAA.txt", params);
+        auto f3 = tree->AddFile(d1, "souborAAAA.txt", params);
+
+        tree->SaveTree();
 	} catch(FenixBackup::FenixException &ex) {
 		cerr << ex.what();
 		return(EXIT_FAILURE);
 	}
 
-    std::ofstream os("test.test");
+    /*std::ofstream os("test.test");
     cereal::JSONOutputArchive archive(os);
 
     auto FI = make_shared<FenixBackup::FileInfo>();
     FenixBackup::FileInfo FI2;
 
-    archive(FI2);
+    archive(FI);*/
 
 	return(EXIT_SUCCESS);
 }
