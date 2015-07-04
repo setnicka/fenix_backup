b5c89903902ff46ba3b2a675b12d17698f2a2b66
Jiří Setnička <setnicka@seznam.cz>
2015-07-04 19:25:55 +0200

    Development: Testing new adapters

--- src/fenix_tester.cpp
+++ src/fenix_tester.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include "adapters/LocalFilesystemAdapter.hpp"
 #include "FileTree.hpp"
 #include "FileChunk.hpp"
 #include "FenixExceptions.hpp"
@@ -18,28 +19,40 @@ int main() {
 
 	try {
 		FenixBackup::Config::Load("/home/jirka/bc_FenixBackup/test/config");
-		FenixBackup::FileTree::GetHistoryTreeList();
-        auto tree = new FenixBackup::FileTree();
+		// FenixBackup::FileTree::GetHistoryTreeList();
 
-        FenixBackup::file_params params = {};
-        params.file_size = 12;
-        auto f1 = tree->AddFile(tree->GetRoot(), "souborB.txt", params);
-        auto d1 = tree->AddDirectory(tree->GetRoot(), "slozka", params);
-        auto f2 = tree->AddFile(d1, "souborAA.txt", params);
-        auto f3 = tree->AddFile(d1, "souborAAAA.txt", params);
+        // 1. Get adapter and run it -> non-saved FileTree
+        auto adapter = new FenixBackup::LocalFilesystemAdapter();
+        adapter->SetPath("/home/jirka/bc_FenixBackup/");
+        auto tree = adapter->Scan();
 
-        tree->SaveTree();
+        // 2. Get files list
+        auto files = tree->FinishTree();
 
-        istringstream ss("ABCDEFGAAAAAABBBBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-        ifstream ifs;
-        ifs.open("/home/jirka/bc_FenixBackup/test/config");
-        tree->ProcessFileContent(f1, ss);
+        // 3. Foreach file in the file list, get file content and process it
+        for (auto& file: files) adapter->GetAndProcess(file);
 
 
-        tree->GetFileContent(f1, cout);
+        //FenixBackup::file_params params = {};
+        //params.file_size = 12;
+        //auto f1 = tree->AddFile(tree->GetRoot(), "souborB.txt", params);
+        //auto d1 = tree->AddDirectory(tree->GetRoot(), "slozka", params);
+        //auto f2 = tree->AddFile(d1, "souborAA.txt", params);
+        //auto f3 = tree->AddFile(d1, "souborAAAA.txt", params);
 
-        auto chunk = FenixBackup::FileChunk::GetChunk("2015-06-29_225718_1");
-        if (chunk) chunk->DeleteChunk();
+        //tree->SaveTree();
+
+        //istringstream ss("ABCDEFGAAAAAABBBBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        //ifstream ifs;
+        //ifs.open("/home/jirka/bc_FenixBackup/test/config");
+        //tree->ProcessFileContent(f1, ss);
+
+
+
+        //tree->GetFileContent(f1, cout);
+
+        //auto chunk = FenixBackup::FileChunk::GetChunk("2015-06-29_225718_1");
+        //if (chunk) chunk->DeleteChunk();
 
 	} catch(FenixBackup::FenixException &ex) {
 		cerr << ex.what();
